e95dc9f2a1a7013f0c430b5d18277f93165ca0b8
Tamir <tamirzehavi@gmail.com>
2022-02-22 19:37:03 +0200

    Editing SA mono

--- NSW_data_analysis/data_sensitivity_analyses/data_SA_mono_CI.R
+++ NSW_data_analysis/data_sensitivity_analyses/data_SA_mono_CI.R
@@ -1,6 +1,6 @@
 #func: predictions for units from O(0,1), plugging A={0,1}
 #########################################################################################
-SACE_estimation_1LEARNER_mono = function(matched_data, reg_after_match, alpha0_mono=1, eta=0, 
+SACE_estimation_1LEARNER_mono = function(matched_data, reg_after_match, alpha0_mono=1, xi=0, 
                                     coeffs_regression_one_model, coeffs_regression_two_models, two_models_bool = TRUE){
   # func: weighted SE with replacements
   func_est_wtd_var_matching_w_rep = function(matched_data, f_alpha){ # f_alpha=1 gives regular weighted var
@@ -18,8 +18,7 @@ SACE_estimation_1LEARNER_mono = function(matched_data, reg_after_match, alpha0_m
   # crude diff estimator, adjusted by alpha0_mono
   A1_S1_data = filter(matched_data, A==1)
   A0_S1_data = filter(matched_data, A==0)
-  #attach(A1_S1_data); #attach(A0_S1_data)
-  f_alpha0 = (1 + eta) / (1 + (eta * alpha0_mono)) 
+  f_alpha0 = (1 + xi) / (1 + (xi * alpha0_mono)) 
   
   crude_Y0_adj = A0_S1_data$Y * f_alpha0
   crude_est_adj = mean(A1_S1_data$Y) - mean(crude_Y0_adj)
@@ -32,7 +31,6 @@ SACE_estimation_1LEARNER_mono = function(matched_data, reg_after_match, alpha0_m
   coeffs_wout_inter = coeffs_regression_wout_inter[-grep("A", names(coeffs_regression_wout_inter))]
   vcov_beta_wout_inter = coeffs_regression_one_model$vcov_wout_interactions
   # predictions wout interactions
-  #print(apply(matched_data, 2, class))
   A0_S1_data$Y1_pred =  coeff_A_wout_inter + 
     as.matrix(subset(A0_S1_data, select = c("intercept", reg_after_match)) %>% apply(2, as.numeric)) %*% coeffs_wout_inter
   A0_S1_data$Y0_pred = as.matrix(subset(A0_S1_data, select = c("intercept", reg_after_match))) %*% coeffs_wout_inter
@@ -55,11 +53,6 @@ SACE_estimation_1LEARNER_mono = function(matched_data, reg_after_match, alpha0_m
   sum_X_tilde_g_alpha0 = apply(X_tilde_g_alpha0, 2, sum)
   var_sum_units_A0_S1_tilde_g_alpha0 = t(sum_X_tilde_g_alpha0) %*%  vcov_beta_wout_inter %*% sum_X_tilde_g_alpha0 
   SACE_1LEARNER_adj_se = sqrt( (1 / nrow(A0_S1_data)^2) * var_sum_units_A0_S1_tilde_g_alpha0 )
-  # if(alpha0_mono != 1 & eta != 0){
-  #   SACE_1LEARNER_adj_se = sqrt( (1 / nrow(A0_S1_data)^2) * var_sum_units_A0_S1_tilde_g_alpha0 )
-  # }else{
-  #   SACE_1LEARNER_adj_se = as.numeric(coeffs_regression_one_model$se_wout_intercations["A"])
-  # }
   
   # with interactions
   #1. if we calculated 1 regression model
@@ -73,7 +66,6 @@ SACE_estimation_1LEARNER_mono = function(matched_data, reg_after_match, alpha0_m
     coeffs_with_inter_A0 = coeffs_regression_with_inter[-grep(":|A", names(coeffs_regression_with_inter))]
     
     # predictions with interactions
-    #mean_x = apply(subset(A0_S1_data, select = reg_after_match),2, mean)
     A0_S1_data$Y1_pred_inter = intercept_with_inter + coeff_A_with_inter +
       as.matrix(subset(A0_S1_data, select = reg_after_match)) %*% 
       (coeffs_with_inter_A1[-grep(":",names(coeffs_with_inter_A1))] + coeffs_with_inter_A1[grep(":",names(coeffs_with_inter_A1))])
@@ -111,7 +103,7 @@ SACE_estimation_1LEARNER_mono = function(matched_data, reg_after_match, alpha0_m
 #########################################################################################
 
 #########################################################################################
-#TODO xi
+# sensitivity parameters ####
 #bounds for xi
 p1 = mean(data[A==1,S]); p0 = mean(data[A==0,S])
 up_bound_xi = (1 - p1) / (p0 - (1 - p1))
@@ -122,22 +114,22 @@ xi_sensi_mono_names = paste0("xi_mono_", round(xi_sensi_mono_vec, 2))
 alpha0_mono_vec = seq(0.5, 2, 0.25) 
 alpha0_mono_vec_names = paste0("alpha0_mono_", alpha0_mono_vec)  
 
+# SA for each combinations of xi and alpha_0  and for all distance metrics ####
 reg_sensi_mono <- NULL
 for (j in 1:length(xi_sensi_mono_names)) {
   xi = xi_sensi_mono_vec[j]
-  ##########################################################################
-  tmp = data # data = adjust_data(data, 1000, data_bool=data_bool)
-  #attach(tmp)
+  #########################################################################################
+  tmp = data 
   
-  #TODO EM
+  # EM
   # calculate PS for the current xi
   tmp$g = ifelse( tmp$A==0 & tmp$S==1, "as", ifelse( tmp$A==1 & tmp$S==0, "ns", ifelse(tmp$A==1 & tmp$S==1, "pro", "pro") )  )
   est_ding_lst_SA_mono = xi_PSPS_M_weighting_SA(Z=tmp$A, D=tmp$S,
                         X=as.matrix(subset(tmp, select = covariates_PS)),  
-                        Y=tmp$Y, eta=xi, # eta = 0 implies monotonicity
+                        Y=tmp$Y, eta=xi, # xi = 0 implies monotonicity
                         beta.c = NULL, beta.n = NULL)
   DING_model_assisted_sensi = est_ding_lst_SA_mono$AACE.reg
-  # c(prob.c, prob.d, prob.a, prob.n)
+  # Ding order: c(prob.c, prob.d, prob.a, prob.n)
   PS_est_sensi_mono = est_ding_lst_SA_mono$ps.score
   # adjust the cols the same order as in myEM: my order is: as, ns, pro, har.
   PS_est = data.frame(EMest_p_as = PS_est_sensi_mono[,3], EMest_p_ns = PS_est_sensi_mono[,4],
@@ -148,23 +140,21 @@ for (j in 1:length(xi_sensi_mono_names)) {
   tmp$e_0_as = tmp$EMest_p_as / (tmp$EMest_p_as + tmp$EMest_p_har)
   tmp = data.table(subset(tmp, select = -c(e_1_as, e_0_as)),  subset(tmp, select = c(e_1_as, e_0_as)))
   ##########################################################################
-  
-  # matching for the current xi
-  #####################################
-  # seed because otherwise the matching procedure will not achieve the same results when alpha_0=1 under all values of xi, during the SA for monotonicity (for mahalanobis measure) 
+
+  # matching for the current xi ####
+  # set.seed because otherwise the matching procedure will not yield the same results when alpha_0=1 under all values of xi, during the SA for monotonicity (for mahalanobis measure) 
   # also, otherwise, when xi=0 in SA for monotonicity results will not be similar to results when alpha_1=1 in SA for PPI (for mahalanobis measure)
   set.seed(101) 
-  matching_lst = matching_func_multiple_data(match_on = match_on,
-           cont_cov_mahal = cont_cov_mahal,  reg_cov = reg_after_match, X_sub_cols = variables, 
-           reg_BC = reg_BC, m_data = tmp[S==1], 
-           w_mat_bool = "NON-INFO", M=1, replace=TRUE, estimand = "ATC", mahal_match = 2, caliper = caliper
-           #, OBS_table = descrip_all_data_OBS$OBS_table
-           , change_id=TRUE, boost_HL=FALSE, pass_tables_matched_units=FALSE, one_leraner_bool=TRUE)
+  matching_lst = matching_func_multiple_data(match_on = match_on
+           ,cont_cov_mahal = cont_cov_mahal,  reg_cov = reg_after_match, X_sub_cols = variables
+           ,reg_BC = reg_BC, m_data = tmp[S==1]
+           ,w_mat_bool = "NON-INFO", M=1, replace=TRUE, estimand = "ATC", mahal_match = 2, caliper = caliper
+           ,change_id=TRUE, boost_HL=FALSE, pass_tables_matched_units=FALSE, one_leraner_bool=TRUE)
  
   #TODO matched_set_lst for all distance metrics
   matched_data_lst = matching_lst$matched_set_lst
   reg_matched_lst = matching_lst$reg_data_matched_lst
-  #TODO run on all distance metrics
+  # run on all distance metrics
   for(ind_matched_set in c(1:length(reg_matched_lst))){  
     matched_data = matched_data_lst[[ind_matched_set]]
     reg_data_matched_SA = reg_matched_lst[[ind_matched_set]]
@@ -175,34 +165,26 @@ for (j in 1:length(xi_sensi_mono_names)) {
       data_reg=matched_data, reg_after_match=reg_after_match) 
     coeffs_regression_two_models = regression_function_two_models(reg_data_matched_SA=reg_data_matched_SA,
       data_reg=matched_data, reg_after_match=reg_after_match) 
-    #########################################################################################
     
     for (i in 1:length(alpha0_mono_vec_names)) {
       print(alpha0_mono_vec_names[i])
       alpha0 = alpha0_mono_vec[i]; alpha1 = 1
       
-      # DING estimator
-      # est_ding_lst = PSPS_M_weighting(Z=data_with_PS$A, D=data_with_PS$S, 
-      #   X=as.matrix(subset(data_with_PS, select = covariates_PS)), Y=data_with_PS$Y,
-      #   trc = TRUE, ep1 = alpha0_mono_vec[i], ep0 = 1, beta.a = NULL, beta.n = NULL, iter.max = iterations , error0 = epsilon_EM)
-      # #DING_est_sensi_PPI = est_ding_lst$AACE
-      # DING_model_assisted_sensi_PPI = est_ding_lst$AACE.reg
-      
-      
       # ONE-LEARNER regression approach
       # predictions for units from O(0,1), plugging A={0,1} + 4. sensitivity adjustments
       reg_sensi_mono =
         rbind( reg_sensi_mono, c(measure = names(matched_data_lst)[ind_matched_set], xi_mono = xi, alpha0_mono = alpha0, 
-        unlist(SACE_estimation_1LEARNER_mono(matched_data=matched_data, reg_after_match=reg_after_match, alpha0_mono=alpha0, eta=xi, 
+        unlist(SACE_estimation_1LEARNER_mono(matched_data=matched_data, reg_after_match=reg_after_match, alpha0_mono=alpha0, xi=xi, 
         coeffs_regression_one_model=coeffs_regression_one_model, coeffs_regression_two_models=coeffs_regression_two_models, two_models_bool=TRUE))) )
       
       print(paste0(xi_sensi_mono_names[j], " ", alpha0_mono_vec_names[i]))
     }
   }
 }
+#########################################################################################
 
-# process for ggplot ####
 #########################################################################################\
+# process for ggplot ####
 reg_sensi_mono = data.frame(reg_sensi_mono)
 reg_sensi_mono[,-1] = apply(reg_sensi_mono[,-1] , 2, as.numeric)
 reg_sensi_mono[,-c(1,2,3)] = round(reg_sensi_mono[,-c(1,2,3)])
@@ -218,7 +200,7 @@ reg_sensi_mono$Estimator = mgsub(reg_sensi_mono$Estimator,
                                  c("crude_est_adj", "SACE_1LEARNER_adj", "SACE_1LEARNER_inter_adj"), legend_levels)
 reg_sensi_mono$Estimator = factor(reg_sensi_mono$Estimator, levels = legend_levels)
 reg_sensi_mono$set = data_bool
-
+#########################################################################################
 
 #########################################################################################
 # plot SA for monotonicity under PPI ####
@@ -256,9 +238,8 @@ plot_sensi_mono_DW_LL = plot_sensi_mono +
   ) 
 #########################################################################################
 
-
-
 #########################################################################################
+# plot ####
 reg_sensi_mono$measure = mgsub(as.character(reg_sensi_mono$measure), "_", " ")
 reg_sensi_mono$measure = factor(reg_sensi_mono$measure, levels = c("Mahal", "Mahal PS cal", "PS"))
 reg_sensi_mono_sppi = filter(reg_sensi_mono, alpha0_mono==1)
@@ -304,73 +285,4 @@ as_ggplot(lgnd_plt)
 plot_sensi_woutLGND = plot_sens_byMetric + theme(legend.position = 'none') 
 #########################################################################################
 
-#func: predictions for units from O(0,1), plugging A={0,1}
-# use xi in the identification from proposition 3, which does not requires PSs estimation, or use e_{0,as} which does require
-#identific_xi = TRUE
-#########################################################################################
-SACE_estimation_1LEARNER_mono_one_model = function(matched_data, coeffs_regression, reg_after_match, 
-                                                   eps_sensi_PPI=1, eps0_sensi_mono=1, eta=0, identific_xi=TRUE){
-  # coefficients
-  # wout interactions
-  coeffs_regression_wout_inter = coeffs_regression$coeffs_wout_intercations
-  coeff_A_wout_inter = coeffs_regression_wout_inter["A"]
-  coeffs_wout_inter = coeffs_regression_wout_inter[-grep("A", names(coeffs_regression_wout_inter))]
-  # with interactions
-  coeffs_regression_with_inter = coeffs_regression$coeffs_with_interactions
-  coeff_A_with_inter = coeffs_regression_with_inter["A"]; intercept_with_inter = coeffs_regression_with_inter["(Intercept)"]
-  coeffs_with_inter_A1 = coeffs_regression_with_inter[-grep("A|(Intercept)", names(coeffs_regression_wout_inter))]
-  coeffs_with_inter_A0 = coeffs_regression_with_inter[-grep(":|A", names(coeffs_regression_with_inter))]
-  
-  # predictions
-  A0_S1_data = filter(matched_data, A==0 & S==1)
-  attach(A0_S1_data)
-  
-  # wout interactions
-  A0_S1_data$Y1_pred = coeff_A_wout_inter + 
-    as.matrix(subset(A0_S1_data, select = c("intercept", reg_after_match))) %*% coeffs_wout_inter
-  A0_S1_data$Y1_pred_adj =  A0_S1_data$Y1_pred / 
-    ( ((1 - eps_sensi_PPI) * A0_S1_data$e_1_as) +  eps_sensi_PPI )
-  
-  A0_S1_data$Y0_pred = as.matrix(subset(A0_S1_data, select = c("intercept", reg_after_match))) %*% coeffs_wout_inter
-  
-  # with interactions
-  #mean_x = apply(subset(A0_S1_data, select = reg_after_match),2, mean)
-  A0_S1_data$Y1_pred_inter = coeff_A_with_inter + intercept_with_inter +
-    as.matrix(subset(A0_S1_data, select = reg_after_match)) %*% 
-    (coeffs_with_inter_A1[-grep(":",names(coeffs_with_inter_A1))] + coeffs_with_inter_A1[grep(":",names(coeffs_with_inter_A1))])
-  A0_S1_data$Y1_pred_inter_adj =  A0_S1_data$Y1_pred_inter / 
-    ( ((1 - eps_sensi_PPI) * A0_S1_data$e_1_as) +  eps_sensi_PPI )
-  
-  A0_S1_data$Y0_pred_inter = as.matrix(subset(A0_S1_data, select=c("intercept", reg_after_match))) %*% coeffs_with_inter_A0
-  
-  # Y0_adj
-  if(identific_xi==TRUE){ # FALSE and TRUE must obtain the same result, F is the same term as T, when expressing eta(or xi) with eps0_sensi_mono
-    A0_S1_data$Y0_pred_adj = (1 + eta) * A0_S1_data$Y0_pred /
-      ( (1 - eps0_sensi_mono) +  ( (1 + eta) * eps0_sensi_mono ) )
-    A0_S1_data$Y0_pred_inter_adj = (1 + eta) * A0_S1_data$Y0_pred_inter /
-      ( (1 - eps0_sensi_mono) +  ( (1 + eta) * eps0_sensi_mono ) )
-  }else if(identific_xi==FALSE){
-    A0_S1_data$Y0_pred_adj =  A0_S1_data$Y0_pred /
-      ( ((1 - eps0_sensi_mono) * A0_S1_data$e_0_as) +  eps0_sensi_mono )
-    A0_S1_data$Y0_pred_inter_adj =  A0_S1_data$Y0_pred_inter /
-      ( ((1 - eps0_sensi_mono) * A0_S1_data$e_0_as) +  eps0_sensi_mono )
-  }
-  
-  # wout inter
-  SACE_1LEARNER = mean(A0_S1_data$Y1_pred) - mean(A0_S1_data$Y0_pred) 
-  SACE_1LEARNER_adj = mean(A0_S1_data$Y1_pred_adj) - mean(A0_S1_data$Y0_pred_adj) 
-  # with inter
-  SACE_1LEARNER_inter = mean(A0_S1_data$Y1_pred_inter) - mean(A0_S1_data$Y0_pred_inter) 
-  SACE_1LEARNER_inter_adj = mean(A0_S1_data$Y1_pred_inter_adj) - mean(A0_S1_data$Y0_pred_inter_adj)
-  
-  # crude diff estimator, adjusted by eps_PPI
-  crude_Y1_adj = matched_data$Y[matched_data$A==1] / 
-    ( ((1 - eps_sensi_PPI) * matched_data$e_1_as[matched_data$A==1]) +  eps_sensi_PPI )
-  crude_Y0_adj = (1 + eta) * matched_data$Y[matched_data$A==0] /
-    ( (1 - eps0_sensi_mono) +  ( (1 + eta) * eps0_sensi_mono ) )
-  crude_est_adj = mean(crude_Y1_adj) - mean(crude_Y0_adj)
-  
-  return(c(SACE_1LEARNER_adj=SACE_1LEARNER_adj, SACE_1LEARNER_inter_adj=SACE_1LEARNER_inter_adj, crude_est_adj=crude_est_adj,
-           SACE_1LEARNER=SACE_1LEARNER, SACE_1LEARNER_inter=SACE_1LEARNER_inter))
-}
-#########################################################################################
+
